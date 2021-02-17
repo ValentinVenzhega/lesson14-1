3da61f6b0a63566197dbe7d90ead7e84a0a2f4a2
Valentin Venzhega <valek917@yandex.ru>
2021-02-17 23:49:53 +0300

    задание 14 выполнено

--- script.js
+++ script.js
@@ -14,23 +14,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
             topTop = 0;
          div.style.cssText = `background: ${this.bg}; height: ${this.height}; width: ${this.width}; position: ${this.position}`;
          document.body.append(div);
-         document.addEventListener('keydown', function moveLeft(event){
-            if(event.code == 'ArrowLeft'){
-               left=left-10;
-               div.style.left = left + 'px';
-            } else if(event.code == 'ArrowRight'){
-                  left=left+10;
-                  div.style.left= left + 'px';
-            } else if(event.code == 'ArrowUp'){
-               topTop=topTop-10;
-               div.style.top= topTop + 'px';
-            } else if(event.code == 'ArrowDown'){
-               topTop=topTop+10;
-               div.style.top= topTop + 'px';
-            }
-         });
+         const move = (event) => {
+            switch(event.code) {
+               case 'ArrowLeft':
+                  if (left > 0) {
+                     left = left - 10;
+                     div.style.left = left + 'px';
+                  } else {
+                     left = 0;
+                     div.style.left = left + 'px';
+                  }
+                  break;
+               case 'ArrowRight':
+                  left = left + 10;
+                  div.style.left = left + 'px';
+                  break;
+               case 'ArrowUp':
+                  if (topTop > 0) {
+                     topTop = topTop - 10;
+                     div.style.top = topTop + 'px';
+                  } else {
+                     topTop = 0;
+                     div.style.top = topTop + 'px';
+                  }
+                  break;
+               case 'ArrowDown':
+                  topTop  = topTop + 10;
+                  div.style.top = topTop + 'px';
+                  break;
+               }
+         };
+         document.addEventListener('keydown', move);
       } 
    }
    const domElement = new DomElement('green', '100px', '100px', 'absolute');
    domElement.getCreate();
 });
+
+
+
+// console.log( document.documentElement.clientWidth);
+// console.log( document.documentElement.clientHeight);
+
+// var scrollHeight = Math.max(
+//    document.documentElement.clientWidth;
+//    document.documentElement.clientHeight;
+//  );
+ 
+//  alert( 'Высота с учётом прокрутки: ' + scrollHeight );
+
+//  var intElemClientWidth = document.documentElement.clientWidth;
+//  var intElemClientHeight = document.documentElement.clientHeight;
+// alert(intElemClientWidth);
+// alert(intElemClientHeight);
