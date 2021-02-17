3646edfb4885e158fa3585995520ba85f6e9613d
Valentin Venzhega <valek917@yandex.ru>
2021-02-17 21:30:53 +0300

    задание 14 доп выыполнил

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 'use strict';
 
-class DomElement {
-   constructor(selector, height, width, bg, fontSize) {
-      this.selector = selector;
-      this.height = height;
-      this.width = width;
-      this.bg = bg;
-      this.fontSize = fontSize;
-   }
-   getCreate () {
-      if (this.selector[0] === '.') {
-         const div = document.createElement('div');
-         document.body.append(div);
-         div.classList.add( this.selector.substring(1));
-         div.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
-         div.style.cssText = `background: ${this.bg}; height: ${this.height}; width: ${this.width}; font-size: ${this.fontSize}`;
-      } else if (this.selector[0] === '#') {
-         const p = document.createElement('p');
-         document.body.append(p);
-         p.id = this.selector.substring(1);
-         p.textContent = 'Maxime deleniti id, velit similique voluptatibus iure doloribus ab a quam.';
-         p.style.cssText = `background: ${this.bg}; height: ${this.height}; width: ${this.width}; font-size: ${this.fontSize}`;
+document.addEventListener("DOMContentLoaded", function(event) {
+   class DomElement {
+      constructor(bg, height, width, position) {
+         this.bg = bg;
+         this.height = height;
+         this.width = width;
+         this.position = position;
       }
-   } 
-}
-
-const domElement = new DomElement('#block', '60px', '600px', 'green', '25px');
-domElement.getCreate();
+      getCreate () {
+         const div = document.createElement('div');
+         let left = 0,
+            topTop = 0;
+         div.style.cssText = `background: ${this.bg}; height: ${this.height}; width: ${this.width}; position: ${this.position}`;
+         document.body.append(div);
+         document.addEventListener('keydown', function moveLeft(event){
+            if(event.code == 'ArrowLeft'){
+               left=left-10;
+               div.style.left = left + 'px';
+            } else if(event.code == 'ArrowRight'){
+                  left=left+10;
+                  div.style.left= left + 'px';
+            } else if(event.code == 'ArrowUp'){
+               topTop=topTop-10;
+               div.style.top= topTop + 'px';
+            } else if(event.code == 'ArrowDown'){
+               topTop=topTop+10;
+               div.style.top= topTop + 'px';
+            }
+         });
+      } 
+   }
+   const domElement = new DomElement('green', '100px', '100px', 'absolute');
+   domElement.getCreate();
+});
